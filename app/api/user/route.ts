2f6ab99db5645823f099b2382039d89a19bbf437
Erick Eduardo <erickepeixoto@gmail.com>
2023-11-02 17:52:23 -0300

    feature: add endpoints resources

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,25 +1,22 @@
 import { db } from "@/db"
 import { NextResponse } from "next/server";
 
-
-
 export async function POST(req: Request) {
 
     try {
         const body = await req.json();
         const { name } = body;
-        const companion = await db.user.create({
+        const user = await db.user.create({
             data: {
               name,
             }
           });
   
-          return NextResponse.json(companion);
+          return NextResponse.json(user);
     } catch (error: any) {
         return new NextResponse(`User Error: ${error.message}`, { status: 400 })
     }
 }
-
 
 export async function GET() {
     try {

7b598d571ba243f678fe32edcace587380fe56e5
Erick Eduardo <erickepeixoto@gmail.com>
2023-11-02 16:27:08 -0300

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section className='py-36'>
+      <div className='container flex items-center justify-center'>
+          Main Page
+      </div>
+    </section>
+  )
+}
